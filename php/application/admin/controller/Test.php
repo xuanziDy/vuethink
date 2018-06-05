0dd9b69172089841d100b2927113328c7acbb04f
ying.deng <827352340@qq.com>
2018-06-05 18:21:26 +0800

    update

---
+++ php/application/admin/controller/Test.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | Description: 部门控制器
+// +----------------------------------------------------------------------
+// | Author: linchuangbin <linchuangbin@honraytech.com>
+// +----------------------------------------------------------------------
+
+namespace app\admin\controller;
+
+use think\Request;
+use think\Db;
+use app\common\adapter\AuthAdapter;
+use app\common\controller\Common;
+
+
+class Test extends Common
+{
+    public function  index()
+    {
+
+        echo "Hello Vuethink";
+    }
+} 
